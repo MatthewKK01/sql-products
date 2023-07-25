9d5b87e7be19a07117097736e2fb13eec00e2492
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-07-25 15:03:45 +0400

    plain js

--- src/index.js
+++ src/index.js
@@ -18,31 +18,31 @@ const serverStart = () => {
   app.use(cors());
   app.use(bodyParser.json);
   app.get("/", async (res, req) => {
-    return res.send("<h1>Welcome to my API</h1>");
+    return await res.send("<h1>Welcome to my API</h1>");
   });
 
-  app.get("/api/products", async (_, res) => {
-    try {
-      const query = await pool.query("SELECT * from products");
-      const rows = query.rows;
-      return res.status(200).json(rows);
-    } catch (error) {
-      return res.status(400).json(error);
-    }
-  });
-  app.post("/api/products", async (req, res) => {
-    const { title, price } = req.body;
-    try {
-      const query = await pool.query(
-        "INSERT INTO products(title, price) VALUES ($1, $2)",
-        [title, price]
-      );
-      const row = query.rows[0];
-      return res.status(201).json(row);
-    } catch (error) {
-      return res.status(401).json(error);
-    }
-  });
+  // app.get("/api/products", async (_, res) => {
+  //   try {
+  //     const query = await pool.query("SELECT * from products");
+  //     const rows = query.rows;
+  //     return res.status(200).json(rows);
+  //   } catch (error) {
+  //     return res.status(400).json(error);
+  //   }
+  // });
+  // app.post("/api/products", async (req, res) => {
+  //   const { title, price } = req.body;
+  //   try {
+  //     const query = await pool.query(
+  //       "INSERT INTO products(title, price) VALUES ($1, $2)",
+  //       [title, price]
+  //     );
+  //     const row = query.rows[0];
+  //     return res.status(201).json(row);
+  //   } catch (error) {
+  //     return res.status(401).json(error);
+  //   }
+  // });
   app.listen(3000);
 };
 
