7ed02967763c16d3eaabfcd27829e8c10b07bc7a
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-07-25 15:16:37 +0400

    fixed everything : () mimaklda es -_-

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const serverStart = () => {
+  app.get("/api/products", async (_, res) => {
+    try {
+      const query = await pool.query("SELECT * from products");
+      const rows = query.rows;
+
+      return res.status(200).json(rows);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  });
+  app.post("/api/products", async (req, res) => {
+    const { title, price } = req.body;
+    try {
+      const query = await pool.query(
+        "INSERT INTO products(title, price) VALUES ($1, $2)",
+        [title, price]
+      );
+      const row = query.rows[0];
+      return res.status(201).json(row);
+    } catch (error) {
+      return res.status(401).json(error);
+    }
+  });
+  app.listen(3000);
+};
 
 const Init = async () => {
   try {
@@ -12,38 +41,6 @@ const Init = async () => {
   } catch (error) {
     console.log(error);
   }
-};
-
-const serverStart = () => {
-  app.use(cors());
-  app.use(bodyParser.json);
-  app.get("/", async (res, req) => {
-    return await res.send("<h1>Welcome to my API</h1>");
-  });
-
-  // app.get("/api/products", async (_, res) => {
-  //   try {
-  //     const query = await pool.query("SELECT * from products");
-  //     const rows = query.rows;
-  //     return res.status(200).json(rows);
-  //   } catch (error) {
-  //     return res.status(400).json(error);
-  //   }
-  // });
-  // app.post("/api/products", async (req, res) => {
-  //   const { title, price } = req.body;
-  //   try {
-  //     const query = await pool.query(
-  //       "INSERT INTO products(title, price) VALUES ($1, $2)",
-  //       [title, price]
-  //     );
-  //     const row = query.rows[0];
-  //     return res.status(201).json(row);
-  //   } catch (error) {
-  //     return res.status(401).json(error);
-  //   }
-  // });
-  app.listen(3000);
 };
 
 Init();
